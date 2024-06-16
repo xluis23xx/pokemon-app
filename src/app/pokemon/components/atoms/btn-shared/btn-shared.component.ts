c87fcafce97eb7191d32efdc5794336f41e1abe7
lycann96 <ltirado@unitru.edu.pe>
2024-06-16 07:53:09 -0500

    feat: shared with whatsapp

--- src/app/pokemon/components/atoms/btn-shared/btn-shared.component.ts
+++ src/app/pokemon/components/atoms/btn-shared/btn-shared.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IResult } from 'src/app/pokemon/models/pokemon.interface';
 
 @Component({
   selector: 'app-btn-shared',
@@ -6,8 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./btn-shared.component.scss']
 })
 export class BtnSharedComponent {
+  @Input() pokemonShared: IResult = {} as IResult;
 
   sharedTo(): void {
-    console.log('sharedTo');
+    const link = this.generateWhatsAppLink();
+    window.open(link, '_blank');
+  }
+
+  generateWhatsAppLink(): string {
+    const message = `Check out this Pokémon: ${this.pokemonShared.name}`;
+    const encodedMessage = encodeURIComponent(message);
+    return `https://wa.me/?text=${encodedMessage}`;
   }
 }
